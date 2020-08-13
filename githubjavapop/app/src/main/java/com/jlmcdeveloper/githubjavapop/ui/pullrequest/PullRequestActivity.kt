32b3f871c535c96dabcc1109da94ead46feaaa0f
JulioMCDias <juliomarques11@gmail.com>
2020-08-13 20:43:20 -0300

    created view PullRequestActivity

--- githubjavapop/app/src/main/java/com/jlmcdeveloper/githubjavapop/ui/pullrequest/PullRequestActivity.kt
+++ githubjavapop/app/src/main/java/com/jlmcdeveloper/githubjavapop/ui/pullrequest/PullRequestActivity.kt
@@ -1,22 +1,21 @@
-package com.jlmcdeveloper.githubjavapop.ui.main
+package com.jlmcdeveloper.githubjavapop.ui.pullrequest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.jlmcdeveloper.githubjavapop.R
-import com.jlmcdeveloper.githubjavapop.databinding.ActivityMainBinding
+import com.jlmcdeveloper.githubjavapop.databinding.ActivityPullRequestBinding
 import org.koin.android.ext.android.inject
 
-
-class MainActivity : AppCompatActivity() {
-    private val viewModel: MainViewModel  by inject()
+class PullRequestActivity : AppCompatActivity() {
+    private val viewModel: PullRequestViewModel by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val binding: ActivityPullRequestBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_pull_request)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
-
     }
 }
---
+++ githubjavapop/app/src/main/java/com/jlmcdeveloper/githubjavapop/ui/pullrequest/PullRequestViewModel.kt
@@ -0,0 +1,11 @@
+package com.jlmcdeveloper.githubjavapop.ui.pullrequest
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.jlmcdeveloper.githubjavapop.data.Repository
+
+class PullRequestViewModel(val repository: Repository) : ViewModel(){
+    val textTitle = MutableLiveData<String?>()
+
+
+}
