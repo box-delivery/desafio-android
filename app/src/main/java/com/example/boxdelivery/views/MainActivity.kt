0d96eeed5491ad7d0757d7ac8af44e8e1f7452a0
Lucas Motta <lucasmottacp@gmail.com>
2020-09-25 12:47:32 -0300

    Implementação de layout e configuração parcial dos arquivos build.gradle

---
+++ app/src/main/java/com/example/boxdelivery/views/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.boxdelivery.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.boxdelivery.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+}
